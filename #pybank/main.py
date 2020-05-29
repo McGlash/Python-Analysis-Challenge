d923312098ae0da1fc3d638584a1566b4162b0e5
McGlash <64424813+McGlash@users.noreply.github.com>
2020-05-29 11:46:16 -0400

    70 percent complete

---
+++ #pybank/main.py
@@ -0,0 +1,104 @@
+#modules to import and read file
+import os
+import csv
+
+# Create file path (Assumes you are in the Python-challenge parent folder)
+budget_file_path = os.path.join ("..", "Resources", "budget_data.csv")
+
+#create holder lists
+
+months = []
+profit_losses = []
+
+#Create holder variables for total calculations
+
+total_months = 0
+total = 0
+
+#Open file, read and view file
+
+with open(budget_file_path, "r") as budget_csvfile:
+    budget_csv_read_file = csv.reader(budget_csvfile, delimiter=',')
+   
+    header = next(budget_csv_read_file)
+
+    #print(header)
+
+    for row in budget_csv_read_file:
+        
+        #total analysis
+        total_months += 1
+        total += int(row[1])
+
+        #create lists per column
+        profit_losses.append(int(row[1]))
+        months.append(row[0])
+
+#determining month-to-month change
+change = []
+
+for month in months:
+    change.append(profit_losses[months.index(month)] - profit_losses[months.index(month)-1])
+
+#remove first calculated value - not valid
+change[0] = 0
+
+#find the average change
+total_change = 0
+
+for calculated_change in change:
+    total_change = total_change + calculated_change
+
+average_change = total_change/(len(change)-1)
+
+#determine greatest decrease/increase
+
+greatest_decrease = 0
+month_of_greatest_decrease = 0
+greatest_increase = 0
+month_of_greatest_increase = 0
+
+for calculated_change_1 in change:
+    if calculated_change_1 > greatest_increase:
+        greatest_increase = calculated_change_1
+        month_of_greatest_increase = months[change.index(calculated_change_1)]
+    if calculated_change_1 < greatest_decrease:
+        greatest_decrease = calculated_change_1
+        month_of_greatest_decrease = months[change.index(calculated_change_1)]
+
+#display findings on terminal
+
+#Title
+print(" ")
+print ("Financial Analysis")
+print("______________________")
+print(" ")
+
+#display the total findings
+print(f"Total month: {total_months}")
+print(f"Total: {total}")
+
+#display the total findings
+print(f"Average change: ${round(average_change, 2)}")
+
+#display the greatest increase/decrease findings
+print(f"Greatest Increase in Profits: {month_of_greatest_increase} (${round(greatest_increase, 2)})")
+print(f"Greatest Decrease in Profits: {month_of_greatest_decrease} (${round(greatest_decrease, 2)})")
+print("______________________")
+
+#store findings 
+title = (" " + "\n" + "Financial Analysis" + "\n" + "______________________" + "\n")
+total_findings = ("Total month: " + str(total_months) + "\n" + "Total: " + str(total) + "\n")
+average_findings =("Average change: $" + str(round(average_change, 2)) + "\n")
+increase_findings = ("Greatest Increase in Profits: " + str(month_of_greatest_increase) + " ($" + str(round(greatest_increase, 2)) + ")" + "\n")
+decrease_findings = ("Greatest Decrease in Profits: " + str(month_of_greatest_decrease) + " ($" + str(round(greatest_decrease, 2)) + ")" + "\n" + "______________________")
+
+#defining path for output file
+budget_outputfile_path = os.path.join ("..", "Analysis", "budget_output.txt")
+
+with open(budget_outputfile_path, "w") as budget_txt_file:
+    budget_txt_file.write(title)
+    budget_txt_file.write(total_findings)
+    budget_txt_file.write(average_findings)
+    budget_txt_file.write(increase_findings)
+    budget_txt_file.write(decrease_findings)
---
+++ #pypoll/main.py
@@ -0,0 +1,96 @@
+#Title
+print(" ")
+print ("Election Results")
+print("_________________________________")
+
+#modules to import and read file
+import os
+import csv
+
+# Create file path (Assumes you are in the #pypoll folder)
+poll_file_path = os.path.join("..", "Resources", "election_data.csv")
+
+#create holder list for votes
+vote = []
+
+#Create holder variables
+total_no_votes = 0
+vote_count = []
+
+#Open file, read and view file
+with open(poll_file_path) as poll_csvfile:
+    poll_csv_read_file = csv.reader(poll_csvfile, delimiter=',')
+   
+    header = next(poll_csv_read_file)
+
+    for row in poll_csv_read_file:
+        
+        #total analysis
+        total_no_votes += 1
+       
+        #create lists per column
+        vote.append(row[2])
+
+# total votes findings display
+print(" ")
+print(f"Total Votes: {total_no_votes}")
+print("_________________________________")
+print(" ")
+
+#create list of candidates
+candidate_list = []
+
+for name in vote:
+    if name not in candidate_list:
+        candidate_list.append(name)
+
+#create tracker list for vote count
+vote_count = []
+
+for name_1 in range(len(candidate_list)):
+    vote_count.append(0)    
+
+#count the votes per candidate
+for candidate_1 in candidate_list:
+    for name_2 in vote:
+        if name_2 == candidate_1:
+            vote_count[candidate_list.index(candidate_1)] += 1
+
+# calculate percentage of votes
+percent_votes = []
+
+for value in vote_count:
+    percent_votes.append((value/total_no_votes)*100)
+
+#votes by candidate findings display
+for candidate_2 in candidate_list:
+    print(f"{candidate_2}: {round(percent_votes[candidate_list.index(candidate_2)],4)}% ({vote_count[candidate_list.index(candidate_2)]})")
+print("_________________________________")
+print(" ")
+
+#determining the winner
+tracker = 0
+winner = 0
+
+for value_1 in vote_count:
+    if value_1 > tracker:
+        tracker = value_1
+        winner = candidate_list[vote_count.index(value_1)]
+
+#winner findings display   
+print(f"Winner: {winner}")
+print("_________________________________")
+
+#store findings 
+title = (" " + "\n" + "Election Results" + "\n" + "______________________" + "\n")
+total_vote_findings = ("Total Votes: " + str(total_no_votes) + "\n" + "______________________" + "\n")
+#average_findings =("Average change: $" + str(round(average_change, 2)) + "\n")
+winner_findings = ("winner: " + str(winner) + "\n" + "______________________")
+
+#defining path for output file
+poll_outputfile_path = os.path.join ("..", "Analysis", "election results.txt")
+
+with open(poll_outputfile_path, "w") as poll_txt_file:
+    poll_txt_file.write(title)
+    poll_txt_file.write(total_vote_findings)
+    poll_txt_file.write(winner_findings)
